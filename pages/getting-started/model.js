7be91403e73ff78b4a9af878d0ee2e3f3596a6dd
mogoweb <mogoweb@gmail.com>
2019-06-06 11:29:46 +0800

    add getting-started example

---
+++ pages/getting-started/model.js
@@ -0,0 +1,24 @@
+import * as tf from '../../tfjs/tf.min.js'
+
+// Tiny TFJS train / predict example.
+const build_model = epochs => {
+  // Create a simple model.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+  // Generate some synthetic data for training. (y = 2x - 1)
+  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+
+  // Train the model using the data.
+  model.fit(xs, ys, { epochs: epochs }).then(() => {
+    model.predict(tf.tensor2d([20], [1, 1])).print();
+  });
+}
+
+module.exports = {
+  build_model: build_model
+}
---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+  },
+  goGettingStarted: function() {
+    wx.navigateTo({
+      url: '../getting-started/index'
+    })
+  }
+})
